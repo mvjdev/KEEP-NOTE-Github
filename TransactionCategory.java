881400b6916e979b08b8faa802f53ac600711ca0
ToavinaMaria <116385647+mvjdev@users.noreply.github.com>
2023-12-15 09:33:43 +0300

    Update TransactionCategory.java

--- TransactionCategory.java
+++ TransactionCategory.java
@@ -1,23 +1,69 @@
 package project.wallet.repository;
 
-import project.wallet.configs.DbConnect;
 import project.wallet.models.TransactionCategory;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class TransactionCategoryCrudOperations extends DbConnect implements CrudOperations<TransactionCategory> {
+public class TransactionCategoryCrudOperations {
 
-    @Override
+    private Connection connection;
+
+    public TransactionCategoryCrudOperations(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<TransactionCategory> findAll() {
+        List<TransactionCategory> categories = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM transaction_categories");
+
+            while (resultSet.next()) {
+                TransactionCategory category = new TransactionCategory()
+                        .setCategoryId(resultSet.getLong("category_id"))
+                        .setCategoryName(resultSet.getString("category_name"));
+                categories.add(category);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
+    }
+
+    public TransactionCategory delete(TransactionCategory value) {
+        try {
+            Long id = value.getCategoryId();
+            Objects.requireNonNull(id);
+
+            Statement statement = connection.createStatement();
+            int deleted = statement.executeUpdate("DELETE FROM transaction_categories WHERE category_id = " + id);
+
+            if (deleted > 0) {
+                statement.close();
+                return value;
+            }
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
     public TransactionCategory save(TransactionCategory value) {
         try {
             String categoryName = value.getCategoryName();
+            Statement statement = connection.createStatement();
             String insertQuery = "INSERT INTO transaction_categories (category_name) VALUES ('" + categoryName + "')";
 
-            Connection connection = getConnection();
-            Statement statement = connection.createStatement();
             int affectedRows = statement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
 
             if (affectedRows == 0) {
@@ -34,60 +80,10 @@ public class TransactionCategoryCrudOperations extends DbConnect implements Crud
 
             generatedKeys.close();
             statement.close();
-            connection.close();
 
             return value;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public List<TransactionCategory> findAll() {
-        List<TransactionCategory> categories = new ArrayList<>();
-        try {
-            Connection connection = getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM transaction_categories");
-
-            while (resultSet.next()) {
-                TransactionCategory category = new TransactionCategory()
-                        .setCategoryId(resultSet.getLong("category_id"))
-                        .setCategoryName(resultSet.getString("category_name"));
-                categories.add(category);
-            }
-
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return categories;
-    }
-
-    @Override
-    public TransactionCategory delete(TransactionCategory value) {
-        try {
-            Long id = value.getCategoryId();
-            Objects.requireNonNull(id);
-
-            Connection connection = getConnection();
-            Statement statement = connection.createStatement();
-
-            String deleteQuery = "DELETE FROM transaction_categories WHERE category_id = " + id;
-            int deleted = statement.executeUpdate(deleteQuery);
-
-            if (deleted > 0) {
-                statement.close();
-                connection.close();
-                return value;
-            }
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
     }
 }
