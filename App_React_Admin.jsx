2871312da6d7a95d93e8d8f0fe36d8c8c0bad6e3
ToavinaMaria <116385647+mvjdev@users.noreply.github.com>
2024-04-28 10:51:13 +0300

    Create App_React_Admin.jsx

---
+++ App_React_Admin.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { PostList } from "./posts/PostList";
+import { UserList } from "./posts/UserList";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={UserList}
+      recordRepresentation={"name"}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
